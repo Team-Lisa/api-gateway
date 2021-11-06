dbeff9dc69708b9933895b5b961d85f767ab587d
vikyzambi <viky_zambi95@hotmail.com>
2021-11-06 17:41:36 -0300

    added enpoints for gamification service

---
+++ api/routes/gamification.py
@@ -0,0 +1,64 @@
+from fastapi import APIRouter
+from api.models.responses.message import Message
+from api.controllers.gamification import Gamification
+from api.models.responses.user import UserStatus as UserStatusResponse
+from api.models.responses.trophies import Trophies as TrophiesResponse
+from api.models.responses.points import Points as PointsResponse
+from api.models.requests.points import Points as PointsRequest
+from api.models.requests.minutes import Minutes as MinutesRequest
+from api.models.responses.minutes import Minutes as MinutesResponse
+from api.models.requests.lives import Lives as LivesRequest
+from api.models.responses.lives import Lives as LivesResponse
+from api.models.requests.fastforwards import Fastforwards as FastforwardsRequest
+from api.models.responses.fastforwards import Fastforwards as FastforwardsResponse
+
+
+router = APIRouter(tags=["Gamification"])
+
+@router.get("/users/userstatus", status_code=201, response_model=UserStatusResponse)
+async def get_user_status(email: str = ""):
+    return Gamification.get_user_status(email)
+
+
+@router.get("/trophies", status_code=201, response_model=TrophiesResponse)
+async def get_trophies(email: str = ""):
+    return Gamification.get_trohpies(email)
+
+
+@router.patch("/points", response_model=PointsResponse)
+async def update_user_points(points: PointsRequest,email: str = ""):
+    return Gamification.update_user_points(email,points)
+
+
+@router.patch("/minutes", response_model=MinutesResponse)
+async def update_user_minutes(minutes: MinutesRequest,email: str = ""):
+    return Gamification.update_user_minutes(email,minutes)
+
+
+@router.patch("/lives", response_model=LivesResponse)
+async def update_user_lives(lives: LivesRequest,email: str = ""):
+    return Gamification.update_user_lives(email,lives)
+
+
+@router.patch("/fastforwards", response_model=FastforwardsResponse)
+async def update_user_fastforwards(amount: FastforwardsRequest,email: str = ""):
+    return Gamification.update_user_fastforwards(email,amount)
+
+@router.patch("challenges/{challenge_id/units/{unit_id}/lessons/{lesson_id}/results", response_model=PointsResponse)
+async def update_history_lesson(points: PointsRequest, challenge_id: str, unit_id: str, lesson_id: str, email: str = ""):
+    Gamification.update_history_lesson(challenge_id, unit_id, lesson_id, email)
+    return Gamification.update_user_points(email, points)
+
+@router.patch("challenges/{challenge_id/units/{unit_id}/exam", response_model=PointsResponse)
+async def update_history_lesson(points: PointsRequest, challenge_id: str, unit_id: str, email: str = ""):
+    Gamification.update_history_exam(challenge_id, unit_id, email)
+    return Gamification.update_user_points(email, points)
+
+@router.patch("challenges/{challenge_id}", response_model=Message)
+async def update_history_lesson( challenge_id: str, email: str = ""):
+    return Gamification.update_challenge_completed(challenge_id,email)
+
+
+
+
+
--- api/services/router.py
+++ api/services/router.py
@@ -4,12 +4,22 @@ import os
 class Router:
     EXERCISES_SERVICE = "EXERCISE_SERVICE"
     USER_SERVICES = "USER_SERVICE"
+    GAMIFICATION_SERVICES = "GAMIFICATION_SERVICE"
     TEST_URL = "http://test.com/"
     BASE_URL = "BASE_URL"
     CHALLENGES = "challenges"
     LESSONS = "lessons"
     SESSIONS = "sessions"
     EXAMS = "exams"
+    USER_STATUS = "user_status"
+    TROPHIES = "trophies"
+    STOREITEMS = "storeitems"
+    POINTS = "points"
+    MINUTES = "minutes"
+    LIVES = "lives"
+    FASTFORWARDS = "fastforwards"
+    HISTORY = "history"
+    CHALLENGE_COMPLETED = "challenge_completed"
     SERVICES_INFORMATION = {
         EXERCISES_SERVICE: {
             BASE_URL: os.getenv("EXERCISES_BASE_URL", TEST_URL),
@@ -20,15 +30,34 @@ class Router:
         USER_SERVICES: {
             BASE_URL: os.getenv("USERS_BASE_URL", TEST_URL),
             SESSIONS: "users/sessions"
+        },
+        GAMIFICATION_SERVICES: {
+            BASE_URL: os.getenv("GAMIFICATIONS_BASE_URL", TEST_URL),
+            USER_STATUS: "users",
+            TROPHIES:"trohpies",
+            STOREITEMS:"storeitems",
+            POINTS: "points",
+            MINUTES: "minutes",
+            LIVES: "lives",
+            FASTFORWARDS: "fastforwards",
+            HISTORY: "/users/history/challenges/{}/units/{}",
+            CHALLENGE_COMPLETED:"/users/history/challenges/{}"
+
         }
     }
 
 
     @staticmethod
-    def get_url(service_name, resource, id=None):
+    def get_url(service_name, resource, id_1=None, id_2=None, params = {}):
         base_url = Router.SERVICES_INFORMATION[service_name].get(Router.BASE_URL)
         endpoint = Router.SERVICES_INFORMATION[service_name].get(resource)
-
+        queryparams = "?"
+        if len(params) > 0:
+            for param in params:
+                queryparams += param + "={}&".format(params[param])
+            queryparams = queryparams[:len(queryparams)-2]
+        else:
+            queryparams = ""
         if id is not None:
             endpoint = endpoint.format(id)
 
