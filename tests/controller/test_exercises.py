61235fba7e7a8f1b411315eb309edbbcd076ddba
matfonseca <mfonseca@fi.uba.ar>
2021-11-22 17:02:47 -0300

    add param to /challenges

--- tests/controller/test_exercises.py
+++ tests/controller/test_exercises.py
@@ -4,7 +4,7 @@ import requests_mock
 from api.services.router import Router
 
 
-def test_get_all_exercises():
+def test_get_all_challenges():
     url = Router.get_url(Router.EXERCISES_SERVICE, Router.CHALLENGES)
 
     with requests_mock.Mocker() as m:
@@ -14,6 +14,25 @@ def test_get_all_exercises():
 
         assert response == json
 
+def test_get_all_challenges_published():
+    url = Router.get_url(Router.EXERCISES_SERVICE, Router.CHALLENGES, params={"published": "true"})
+
+    with requests_mock.Mocker() as m:
+        json = {"challenges": []}
+        m.register_uri('GET', url, json=json, status_code=200)
+        response = Exercises.get_challenges(published="true")
+
+        assert response == json
+
+def test_get_all_challenges_not_published():
+    url = Router.get_url(Router.EXERCISES_SERVICE, Router.CHALLENGES, params={"published": "false"})
+
+    with requests_mock.Mocker() as m:
+        json = {"challenges": []}
+        m.register_uri('GET', url, json=json, status_code=200)
+        response = Exercises.get_challenges(published="false")
+
+        assert response == json
 
 def test_get_lesson_exercises():
     lesson_id = "L1"
