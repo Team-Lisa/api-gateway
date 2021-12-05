61c7f715182758c6c53f4e3ee8274881c7cafecb
matfonseca <mfonseca@fi.uba.ar>
2021-12-05 17:09:46 -0300

    add /tracks to gateway

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
-from api.routes import helpers, exercises, users, gamification
+from api.routes import helpers, exercises, users, gamification, tracks
 
 app = FastAPI()
 
@@ -9,7 +9,7 @@ app.include_router(helpers.router)
 app.include_router(exercises.router)
 app.include_router(users.router)
 app.include_router(gamification.router)
-
+app.include_router(tracks.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5001, log_level="info")
